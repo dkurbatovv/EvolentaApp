bd3851bf150e637b225d2c6d9a7d8547b9996c28
Dkurbatovv <kurb.2015@inbox.rugit config --global user.name Dkurbatovvgit config --global user.email kurb.2015@inbox.ru>
2023-09-20 10:34:13 +0300

    Сервисы

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { BooksService } from '../books.service';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent {
+  constructor(
+    public  bookServ: BooksService
+  ) {}
+  title = '1.6-Services';
+}
