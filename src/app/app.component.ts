20c21edb02792e965ca421cefa50d7602fff7b56
Dkurbatovv <kurb.2015@inbox.rugit config --global user.name Dkurbatovvgit config --global user.email kurb.2015@inbox.ru>
2023-09-23 12:14:47 +0300

    Tags

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'EvolentaApp-Tags';
+
+  constructor(private meta: Meta) {
+    this.meta.addTag({ property: 'og:desc', content: 'root_desc' }
+    );
+  }
+}
